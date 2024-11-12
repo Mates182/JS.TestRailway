299e1da7446d934c9cffed2868cdf66dcbada578
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-11-11 19:14:49 -0500

    feat: :sparkles: Edit the index and App.jsx file
    Change the text to a Welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome, This page is deployed on Railway</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
